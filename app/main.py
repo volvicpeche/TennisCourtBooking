3948fd5c5940eaf44caad539084dcd9523e281d3
florian <espla.florian@laposte.net>
2025-06-30 14:30:24 +0200

    Add web UI for tennis court booking

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from .core.database import Base, engine
 from .api import bookings
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Tennis Court Booking")
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(bookings.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
